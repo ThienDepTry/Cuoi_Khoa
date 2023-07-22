ff4a40614c8dcbb302586b22278a0d3a0b7d4960
ThienDepTry <105977700+ThienDepTry@users.noreply.github.com>
2023-07-22 19:48:37 +0700

    PageHome Update

---
+++ data.js
@@ -0,0 +1,34 @@
+const news = [
+    {
+        id: 1,
+        image: "https://images2.thanhnien.vn/Uploaded/2014/ihay.thanhnien.com.vn/Pictures201310/LUAN/2/damvinhhungg6.jpg",
+        category: "News",
+        title: "Creating a Future Worth Living",
+        description: "Learn the attributes you need to gain in order to build a future and create a life that you are truly happy with.aaaaa",
+        author: "me",
+        created_at: Date.now(),
+        readtime: "10min",
+    }
+    ,
+    {
+        id: 2,
+        image: "https://cdn.devdojo.com/images/may2021/fruit.jpg",
+        category: "LIFESTYLE",
+        title: "Creating a Future Worth Living 2",
+        description: "Learn the attributes you need to gain in order to build a future and create a life that you are truly happy with.",
+        author: "me",
+        created_at: Date.now(),
+        readtime: "10min",
+    }
+    ,
+    {
+        id: 3,
+        image: "https://cdn.devdojo.com/images/may2021/fruit.jpg",
+        category: "LIFESTYLE",
+        title: "Creating a Future Worth Living 3",
+        description: "Learn the attributes you need to gain in order to build a future and create a life that you are truly happy with.",
+        author: "me",
+        created_at: Date.now(),
+        readtime: "10min",
+    }
+]
